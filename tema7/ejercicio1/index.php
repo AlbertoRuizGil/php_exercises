92a60d3dec461c6f25561b068cea30ac5c2a54d9
unknown <Usuario@AU8BISPC13.ieszayas.org>
2019-10-17 11:09:56 +0200

    empezado ejercicio1 tema7

---
+++ tema7/ejercicio1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+
+        <h1>procesador de texto</h1>
+    
+        <textarea name="textbox" id="textbox" cols="30" rows="10" placeholder="Escribe tu texto"></textarea>
+        <input type="text" name="searchword" id="searchword">
+        <input type="submit" value="buscar" id="searchbtn">
+    
+    </form>
+</body>
+</html>
