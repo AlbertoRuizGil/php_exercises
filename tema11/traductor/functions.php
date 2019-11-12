4cef4bff08c776109231fe55033d0903606fc48b
Alberto <ruizgilalberto@gmail.com>
2019-11-12 21:31:43 +0100

    ejercicio de traductor del tema11 casi terminado

--- tema11/traductor/functions.php
+++ tema11/traductor/functions.php
@@ -1,34 +1,64 @@
-<?php
+ <?php
 
-    function getJSON(){
+    function getJSON($language){
         $str = file_get_contents('./languages.json');
         $languages = json_decode($str, true);
-        return $languages[$_POST['language']];
+        return $languages[$language];
     }
 
-    function paintForm($language = ""){
+    function paintForm($language){
+
+        $selectedlanguage = getJSON($language);
+        setcookie("language", $language, time()+60);
+        
+        //Pîntamos el formulario de configuración
+        echo <<<EOD
+
+        <div class="language">
+            <form action="" method="post">
+
+                <select name="language">
+                    <option disabled selected>{$selectedlanguage['select']['label']}</option>
+                    <option value="es">{$selectedlanguage['select']['options']['es']}</option>
+                    <option value="en">{$selectedlanguage['select']['options']['en']}</option>
+                </select>
+
+                <select label="font">
+                    <option disabled selected>{$selectedlanguage['font']['label']}</option>
+                    <option value="yellow">{$selectedlanguage['colors']['yellow']}</option>
+                    <option value="blue">{$selectedlanguage['colors']['blue']}</option>
+                </select>
+
+                <select label="background">
+                    <option disabled selected>{$selectedlanguage['background']['label']}</option>
+                    <option value="red">{$selectedlanguage['colors']['red']}</option>
+                    <option value="green">{$selectedlanguage['colors']['green']}</option>
+                </select>
+
+                <input type="submit" value="{$selectedlanguage['submit']['label']}">
+
+            </form>
+        </div>
+EOD;
 
 
+        echo <<<EOD
 
+        <div class="login">
+
+            <form action="" method="post">
+
+                <p>Login</p>
+                <input type="text" name="name" placeholder="{$selectedlanguage['login']['user']}"><br>
+                <input type="password" name="passwd" placeholder="{$selectedlanguage['login']['passwd']}"><br>
+                <input type="submit" name="submit" value="{$selectedlanguage['login']['submit']}">
+
+
+            </form>
+
+        </div>
+EOD;
     }
 
 ?>
 
-<form action="" method="post">
-
-    <select label="/* idioma */">
-        <option value="es">/* texto */</option>
-        <option value="en">/* texto */</option>
-    </select>
-
-    <select label="/* color fuente */">
-        <option value="" label="/* texto */">/* texto */</option>
-        <option value="en" label="/* texto */">/* texto */</option>
-    </select>
-
-    <select label="/* fondo */">
-        <option value="" label="/* texto */">/* texto */</option>
-        <option value="" label="/* texto */">/* texto */</option>
-    </select>
-
-</form>
--- tema11/traductor/index.php
+++ tema11/traductor/index.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
 </head>
 <body>
-    <?php
-
-        require("./functions.php");
-
-        if(!isset($_COOKIE["language"])){
-            $default = "es";
-            p
-        }
-
-    
-
-    ?>
+  <?php
+    require("./functions.php");
+    if(isset($_POST["language"])){
+      paintForm($_POST["language"]);
+    } else if(isset($_COOKIE["language"])){
+      paintForm($_COOKIE["language"]);
+    }else{
+      $default = "es";
+      paintForm($default);
+    }
+  
+  ?>
 </body>
 </html>
 
